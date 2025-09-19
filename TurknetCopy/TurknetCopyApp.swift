b2535498b5516a01d47a43eb7031625c38092744
Barış Demirci <baris19052003@gmail.com>
2025-09-19 14:48:58 +0300

    Initial Commit

---
+++ TurknetCopy/TurknetCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurknetCopyApp.swift
+//  TurknetCopy
+//
+//  Created by Barış Demirci on 19.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TurknetCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
