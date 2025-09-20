1abab7ba54f5e84bf8e98bff35e0d563908e1358
Barış Demirci <baris19052003@gmail.com>
2025-09-20 18:13:52 +0300

    login page created.

--- TurknetCopy/LoginPage.swift
+++ TurknetCopy/LoginPage.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct LoginPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.background
+                .ignoresSafeArea(edges: .all)
+            VStack{
+                HStack{
+                    Image("turknetlogo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 130)
+                        .padding(.bottom, 150)
+                }
+                VStack(spacing: 35){
+                    TextField("Kullanıcı Adı", text: .constant(""))
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 0.5, y: 0.9)
+                    SecureField("Şifre", text: .constant(""))
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 0.5, y: 0.9)
+                }
+                CustomInformation(text: "Güvenliğiniz için sistemde kayıtlı olan telefon numaranıza doğrulama kodu gönderilecektir.")
+                    .padding(.top, 20)
+                
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
